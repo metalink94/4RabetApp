3defc89ab40b8625b940c4077d25367ef61551c3
Denis <denisnovikov94@gmail.com>
2019-02-10 22:49:54 +0300

    add new checks

--- app/src/main/java/ru/app/a4rabetapp/screens/menu/MenuActivity.kt
+++ app/src/main/java/ru/app/a4rabetapp/screens/menu/MenuActivity.kt
@@ -2,8 +2,10 @@ package ru.app.a4rabetapp.screens.menu
 
 import android.content.Context
 import android.content.Intent
+import android.location.Address
 import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import kotlinx.android.synthetic.main.menu.*
 import ru.app.a4rabetapp.R
@@ -12,19 +14,69 @@ import ru.app.a4rabetapp.screens.about.AboutActivity
 import ru.app.a4rabetapp.screens.results.ResultsActivity
 import android.telephony.TelephonyManager
 import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import ru.app.a4rabetapp.base.Features
 import ru.app.a4rabetapp.screens.web.WebActivity
+import java.util.*
+import android.location.Geocoder
+import ru.app.a4rabetapp.base.Utils
+import java.io.IOException
 
 
 class MenuActivity: BaseActivity() {
+
+    private var url: String? = Features.URL
+    private var isNeedStub: Boolean = true
+    private var countryIso: String? = Features.COUNTRY_ISO_CODE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.menu)
+        checkDatabase()
         results.setOnClickListener { onResultsClick() }
         chat.setOnClickListener { onChatClick() }
         about.setOnClickListener { onAboutClick() }
+    }
+
+    private fun checkDatabase() {
+        showProgressView()
+        val database = FirebaseDatabase.getInstance()
+        database.reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.value != null) {
+                    url = p0.child(getString(R.string.key_first)).value as String?
+                    isNeedStub = p0.child(getString(R.string.key_two)).value as Boolean
+                    countryIso = p0.child(getString(R.string.key_country)).value as String?
+                }
+                Log.d("DataBase", "get database ${p0.value}")
+                hideProgressView()
+                showAlertDialog()
+                Log.d("CheckCountry", "User Country ${checkCountry()}")
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("URL", p0.message)
+                hideProgressView()
+            }
+        })
+    }
+
+    private fun showAlertDialog() {
+        // only for debug
+        val tm = this.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val countryCodeValue = tm.networkCountryIso
+        val roaming = tm.isNetworkRoaming
+        Log.d("Menu", " countryCodeValue: $countryCodeValue, " +
+                "roaming $roaming, remote ${countryIso?.toLowerCase()}")
+        AlertDialog.Builder(this)
+                .setTitle("Check Country")
+                .setMessage("Your phoneCountry is ${tm.simCountryIso} your networkCountry is $countryCodeValue and you in roaming $roaming, get ip: ${Utils.getIPAddress(true)}")
+                .setPositiveButton("Ok") { dialog, which ->  }
+                .show()
     }
 
     private fun onAboutClick() {
@@ -37,18 +89,28 @@ class MenuActivity: BaseActivity() {
     }
 
     private fun onResultsClick() {
-        if (!acceptResults() && getRemoteConfig().getBoolean(Features.URL_NEED)) {
-            startActivity(Intent(this, ResultsActivity::class.java))
+        if (!isNeedStub && checkCountry()?.toLowerCase() == countryIso?.toLowerCase()) {
+            startActivity(WebActivity.getInstance(this, url))
         } else {
-            startActivity(Intent(this, WebActivity::class.java))
+            startActivity(Intent(this, ResultsActivity::class.java))
         }
     }
 
-    private fun acceptResults(): Boolean {
-        val tm = this.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        val countryCodeValue = tm.networkCountryIso
-        val roaming = tm.isNetworkRoaming
-        Log.d("Menu", " countryCodeValue: $countryCodeValue, roaming $roaming, remote ${getRemoteConfig().getString(Features.COUNTRY_ISO_CODE).toLowerCase()}")
-        return countryCodeValue.toLowerCase() != getRemoteConfig().getString(Features.COUNTRY_ISO_CODE).toLowerCase() && !roaming
+    private fun checkCountry(): String? {
+        try {
+            val tm = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+            val simCountry = tm.simCountryIso
+            if (simCountry != null && simCountry.length == 2) { // SIM country code is available
+                return simCountry.toLowerCase(Locale.US)
+            } else if (tm.phoneType != TelephonyManager.PHONE_TYPE_CDMA) { // device is not 3G (would be unreliable)
+                val networkCountry = tm.networkCountryIso
+                if (networkCountry != null && networkCountry.length == 2) { // network country code is available
+                    return networkCountry.toLowerCase(Locale.US)
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Error", e.localizedMessage)
+        }
+        return null
     }
 }
--- app/src/main/java/ru/app/a4rabetapp/screens/web/WebActivity.kt
+++ app/src/main/java/ru/app/a4rabetapp/screens/web/WebActivity.kt
@@ -1,24 +1,39 @@
 package ru.app.a4rabetapp.screens.web
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.util.Log
-import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.view.WindowManager
+import android.webkit.*
+import android.widget.Toast
 import kotlinx.android.synthetic.main.main.*
 import ru.app.a4rabetapp.R
-import ru.app.a4rabetapp.app.FourRabetApp
 import ru.app.a4rabetapp.base.BaseActivity
-import ru.app.a4rabetapp.base.Features
+
 
 class WebActivity: BaseActivity() {
 
-
-    var currentUrl: String? = ""
+    var currentUrl: String = ""
+    private var uploadMessage: ValueCallback<Array<Uri>>? = null
+    private var mUploadMessage: ValueCallback<Uri>? = null
+    private val PICK_FROM_GALLERY = 1
+    private val PICK_FROM_GALLERY_2 = 2
+    private val PICK_FROM_GALLERY_3 = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        currentUrl = intent.getStringExtra(KEY_URL)
+        checkPermissions()
         initWebView()
         swipeRefresh.setOnRefreshListener {
             swipeRefresh.isRefreshing = true
@@ -26,20 +41,75 @@ class WebActivity: BaseActivity() {
         }
     }
 
-    private fun getUrl(): String {
-        return if (application is FourRabetApp) {
-            Log.d("FourRabetApp", getRemoteConfig().getString(Features.URL))
-            getRemoteConfig().getString(Features.URL)
-        } else {
-            getString(R.string.url)
+    private fun checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PICK_FROM_GALLERY
+            )
         }
     }
 
+
     private fun initWebView() {
-        webView.webChromeClient = WebChromeClient()
-        webView.webViewClient = object : WebViewClient() {
+        webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowFileChooser(
+                    webView: WebView?, filePathCallback: ValueCallback<Array<Uri>>?,
+                    fileChooserParams: FileChooserParams?
+            ): Boolean {
+                if (uploadMessage != null) {
+                    uploadMessage?.onReceiveValue(null)
+                    uploadMessage = null
+                }
+
+                uploadMessage = filePathCallback
+                var intent: Intent? = null
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    intent = fileChooserParams?.createIntent()
+                }
+                try {
+                    startActivityForResult(intent, PICK_FROM_GALLERY_2)
+                } catch (e: ActivityNotFoundException) {
+                    uploadMessage = null
+                    Toast.makeText(applicationContext, "Cannot Open File Chooser", Toast.LENGTH_LONG).show()
+                    return false
+                }
+
+                return true
+            }
+
+            override fun onJsAlert(view: WebView?, url: String?, message: String?, result: JsResult?): Boolean {
+                Log.d("LogTag", message)
+                result?.confirm()
+                return true;
+            }
+        }
+        webView.webViewClient = object : WvClient() {
+
+            @TargetApi(Build.VERSION_CODES.N)
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                val uri = request?.url
+                return shouldOverrideUrlLoading(uri.toString())
+            }
+
+            @Deprecated("shouldOverrideUrlLoading")
             override fun shouldOverrideUrlLoading(view: WebView, url: String?): Boolean {
+                return shouldOverrideUrlLoading(url ?: "")
+            }
+
+            private fun shouldOverrideUrlLoading(url: String): Boolean {
+                if (url.startsWith("market")) {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                    finish()
+                    return true
+                }
                 currentUrl = url
+                Log.d("WebView", "get Url $currentUrl")
+//                load()
                 return false
             }
 
@@ -48,9 +118,14 @@ class WebActivity: BaseActivity() {
                 swipeRefresh.isRefreshing = false
             }
         }
-        webView.clearCache(true)
+        CookieSyncManager.createInstance(this)
+        val cookieManager = CookieManager.getInstance()
+        cookieManager.setAcceptCookie(true)
+
+//        webView.clearCache(true)
         webView.clearHistory()
         webView.setInitialScale(1)
+        webView.settings.setAppCacheEnabled(true)
         webView.settings.javaScriptEnabled = true
         webView.settings.loadWithOverviewMode = true
         webView.settings.useWideViewPort = true
@@ -59,11 +134,57 @@ class WebActivity: BaseActivity() {
         webView.isScrollbarFadingEnabled = false
         webView.settings.builtInZoomControls = true
         webView.settings.displayZoomControls = false
-        currentUrl = getUrl()
+        webView.settings.domStorageEnabled = true
         load()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        if (newConfig?.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
     }
 
     private fun load() {
         webView.loadUrl(currentUrl)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_FROM_GALLERY_2) {
+            if (uploadMessage == null) return
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                uploadMessage?.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data))
+            }
+            uploadMessage = null
+        } else if (requestCode == PICK_FROM_GALLERY_3) {
+            if (null == mUploadMessage) return
+            val result = if (data == null || resultCode != RESULT_OK) {
+                null
+            } else {
+                data.data
+            }
+            mUploadMessage?.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    companion object {
+        const val KEY_URL = "url"
+
+        fun getInstance(context: Context, url: String?): Intent =
+                Intent(context, WebActivity::class.java)
+                        .putExtra(KEY_URL, url)
     }
 }
