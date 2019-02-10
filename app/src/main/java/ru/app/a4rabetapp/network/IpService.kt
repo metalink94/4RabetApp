adff373865ad56a8897bca0f22f527cbee4198f4
Denis <denisnovikov94@gmail.com>
2019-02-10 23:53:11 +0300

    fix

--- app/src/main/java/ru/app/a4rabetapp/network/IpService.kt
+++ app/src/main/java/ru/app/a4rabetapp/network/IpService.kt
@@ -4,10 +4,11 @@ import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.app.a4rabetapp.models.IpModel
 
 interface IpService {
 
     @GET("{ip}")
     fun getIpLocation(@Path("ip") userIp: String,
-                      @Query("access_key") key: String): Observable<String>
+                      @Query("access_key") key: String): Observable<IpModel>
 }
